2575721dc738d2257edc430128d45066b9d55f0d
Michael Nolan <mtnolan2640@gmail.com>
2020-04-11 10:25:10 -0400

    Add API endpoint for updating a sensor

--- broker/main.py
+++ broker/main.py
@@ -1,11 +1,48 @@
-from flask import Flask
+from flask import Flask, request, abort
+
 
 PORT = 9000
 app = Flask(__name__)
 
+
+class SensorData:
+    def __init__(self, stype, name, value):
+        self.stype = stype
+        self.name = name
+        self.value = value
+
+
+class AppData:
+    def __init__(self):
+        self.sensors = {}
+
+    def update_sensor(self, stype, name, value):
+        self.sensors[name] = SensorData(stype, name, value)
+
+appdata = AppData()
+
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+@app.route('/api/sensor', methods=['POST', 'PUT'])
+def update_sensor():
+    print(request.json)
+    if not request.json:
+        abort(400)
+    if 'sensorid' not in request.json:
+        abort(400)
+    if 'sensortype' not in request.json:
+        abort(400)
+    if 'value' not in request.json:
+        abort(400)
+    sid = request.json.get('sensorid')
+    stype = request.json.get('sensortype')
+    value = request.json.get('value')
+    appdata.update_sensor(stype, sid, value)
+    return "OK"
+
 
 if __name__ == '__main__':
     app.run(debug=False, port=PORT)
--- broker/test_main.py
+++ broker/test_main.py
@@ -1,6 +1,7 @@
 import unittest
+import json
 
-from main import app
+from main import app, appdata
 
 class BrokerTests(unittest.TestCase):
 
@@ -17,6 +18,18 @@ class BrokerTests(unittest.TestCase):
         resp = self.app.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data, b"Hello, World!")
+
+    def test_update_sensor(self):
+        data = {"sensortype": "noise",
+                "sensorid": "1",
+                "value": "-93dB"}
+        resp = self.app.put('/api/sensor', json=data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('1' in appdata.sensors)
+        sensor = appdata.sensors['1']
+        self.assertEqual(sensor.name, '1')
+        self.assertEqual(sensor.stype, 'noise')
+        self.assertEqual(sensor.value, '-93dB')
 
 
 if __name__ == '__main__':
