fa07c547b6048f295636df9aba07f1c32e620de0
Michael Nolan <mtnolan2640@gmail.com>
2020-04-30 16:40:39 -0400

    Rudimentary client that handles changes in both API's data

--- client/client/subscriber.py
+++ client/client/subscriber.py
@@ -2,22 +2,34 @@ import asyncio
 import websockets
 import requests
 import re
+import json
 
 data = '{"cmd": "subscribe", "sensorid": "button"}'
 
 button_event = asyncio.Event()
+button_value = 0
 
 heat_event = asyncio.Event()
 heat_lock = asyncio.Lock()
 heat_value = 0
 
 async def subscribe_to_n4abi():
+    global button_value
     uri = "ws://n4abi.com:9000/api/subscribe"
     async with websockets.connect(uri) as websocket:
         await websocket.send(data)
         while True:
             resp = await websocket.recv()
-            print(resp)
+            jdata = json.loads(resp)
+            print(jdata)
+            status = jdata['value']
+            prev_value = button_value
+            value = 1 if status == 'pressed' else 0
+            print(value, prev_value)
+            button_value = value
+            if value == 1 and prev_value == 0:
+                print("Notifying button_event")
+                button_event.set()
 
 
 async def update_heat(heat):
@@ -39,7 +51,6 @@ async def poll_spencer():
         resp = await fut
         data = resp.text
         data = data.replace('Sensors:\n\n', '')
-        print(data)
         lines = data.splitlines()
         for line in lines:
             if line:
@@ -49,6 +60,32 @@ async def poll_spencer():
         await asyncio.sleep(1)
 
 
+async def handle_fire():
+    heat_event.clear()
+    print("Fire!")
+
+async def handle_reset():
+    button_event.clear()
+    print("Reset!")
+
+async def handle_logic():
+    print("handle logic")
+    while True:
+        h = asyncio.ensure_future(heat_event.wait())
+        b = asyncio.ensure_future(button_event.wait())
+        done, pending = await asyncio.wait({h, b},
+                                           return_when=asyncio.FIRST_COMPLETED)
+        for task in pending:
+            task.cancel()
+        if h in done:
+            await handle_fire()
+        if b in done:
+            await handle_reset()
+
+
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(poll_spencer())
+loop.run_until_complete(asyncio.gather(
+    handle_logic(),
+    subscribe_to_n4abi(),
+    poll_spencer()))
